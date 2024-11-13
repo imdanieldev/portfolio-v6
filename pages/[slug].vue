6ac4f3dd7c9c6c2d952278e886d4657cc9d38ec4
Danial <danialnasronline@gmail.com>
2024-11-13 19:17:33 +0330

    fix 404 page theme

--- pages/[slug].vue
+++ pages/[slug].vue
@@ -2,6 +2,6 @@
 	<div class="calc-height w-full flex justify-center items-center flex-col px-1">
 		<h1 class="text-6xl text-center sm:text-9xl">404</h1>
 		<h2 class="text-4xl text-center sm:text-5xl">Page Not Found</h2>
-		<NuxtLink to="/" class="mt-5 text-3xl bg-black/20 px-4 py-2 border-2 border-solid border-purple-600 cursor-pointer text-center">Back Home</NuxtLink>
+		<NuxtLink to="/" class="mt-5 text-3xl bg-black/20 px-4 py-2 border-2 border-solid border-blue-600 cursor-pointer text-center">Back Home</NuxtLink>
 	</div>
 </template>
